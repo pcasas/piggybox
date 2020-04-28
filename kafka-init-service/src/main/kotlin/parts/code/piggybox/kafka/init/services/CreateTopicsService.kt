c72b661d1a42d484bdc6200121f852e4e1c4532f
Pau Casas Prunés <16187642+casasprunes@users.noreply.github.com>
2020-04-28 23:05:39 +0200

    Update gradle version (#47)

--- kafka-init-service/src/main/kotlin/parts/code/piggybox/kafka/init/services/CreateTopicsService.kt
+++ kafka-init-service/src/main/kotlin/parts/code/piggybox/kafka/init/services/CreateTopicsService.kt
@@ -5,6 +5,7 @@ import javax.inject.Inject
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.admin.KafkaAdminClient
 import org.apache.kafka.clients.admin.NewTopic
+import org.slf4j.LoggerFactory
 import parts.code.piggybox.kafka.init.config.KafkaConfig
 import ratpack.service.Service
 
@@ -12,18 +13,26 @@ class CreateTopicsService @Inject constructor(
     config: KafkaConfig
 ) : Service {
 
+    private val logger = LoggerFactory.getLogger(CreateTopicsService::class.java)
+
     init {
         val properties = Properties().apply {
             put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, config.bootstrapServersConfig)
         }
 
-        KafkaAdminClient.create(properties).createTopics(
-            listOf(
-                NewTopic(config.topics.preferencesAuthorization, 1, 1),
-                NewTopic(config.topics.preferences, 1, 1),
-                NewTopic(config.topics.balanceAuthorization, 1, 1),
-                NewTopic(config.topics.balance, 1, 1)
-            )
-        ).all().get()
+        val client = KafkaAdminClient.create(properties)
+
+        val topics = listOf(
+            config.topics.preferencesAuthorization,
+            config.topics.preferences,
+            config.topics.balanceAuthorization,
+            config.topics.balance
+        ).filter { client.listTopics().names().get().contains(it).not() }
+            .map { NewTopic(it, 1, 1) }
+
+        if (topics.isNotEmpty()) {
+            client.createTopics(topics).all().get()
+            logger.info("Created topic(s): ${topics.joinToString(separator = ", ") { it.name() }}")
+        }
     }
 }
