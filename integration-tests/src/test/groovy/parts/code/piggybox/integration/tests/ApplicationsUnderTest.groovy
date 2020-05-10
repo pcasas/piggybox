07f8017f1d17a32cacc0fc2509785814df87cd98
Pau Casas Prunés <16187642+casasprunes@users.noreply.github.com>
2020-05-10 11:06:35 +0200

    Run apps in Docker (#54)

--- integration-tests/src/test/groovy/parts/code/piggybox/integration/tests/ApplicationsUnderTest.groovy
+++ integration-tests/src/test/groovy/parts/code/piggybox/integration/tests/ApplicationsUnderTest.groovy
@@ -2,28 +2,24 @@ package parts.code.piggybox.integration.tests
 
 import parts.code.piggybox.balance.BalanceServiceApplication
 import parts.code.piggybox.command.CommandServiceApplication
-import parts.code.piggybox.kafka.init.KafkaInitServiceApplication
 import parts.code.piggybox.preferences.PreferencesServiceApplication
 import parts.code.piggybox.query.QueryServiceApplication
 import ratpack.test.MainClassApplicationUnderTest
 
 class ApplicationsUnderTest {
-    final def kafkaInitService = new MainClassApplicationUnderTest(KafkaInitServiceApplication)
     final def commandService = new MainClassApplicationUnderTest(CommandServiceApplication)
     final def preferencesService = new MainClassApplicationUnderTest(PreferencesServiceApplication)
     final def balanceService = new MainClassApplicationUnderTest(BalanceServiceApplication)
     final def queryService = new MainClassApplicationUnderTest(QueryServiceApplication)
 
     boolean started() {
-        return (kafkaInitService.address != null &&
-                commandService.address != null &&
+        return (commandService.address != null &&
                 preferencesService.address != null &&
                 balanceService.address != null &&
                 queryService.address != null)
     }
 
     void close() {
-        kafkaInitService.close()
         commandService.close()
         preferencesService.close()
         balanceService.close()
