718066b879a8a5d1409e6b9939d45db339d7fce1
Pau Casas Prunés <16187642+casasprunes@users.noreply.github.com>
2020-05-30 23:05:50 +0200

    Populate form fields with previously saved data (#60)

--- client/src/Account.js
+++ client/src/Account.js
@@ -12,7 +12,6 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Divider from "@material-ui/core/Divider";
-import CreditCardIcon from "@material-ui/icons/CreditCard";
 import SettingsIcon from "@material-ui/icons/Settings";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 
