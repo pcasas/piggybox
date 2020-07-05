e94549d94281ce0fa9224d0ccf0a515d368eb9f6
casasprunes <admin@code.parts>
2020-07-05 13:43:15 +0200

    Create withdraw funds dialog

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,6 +4,7 @@ import { Container, makeStyles } from "@material-ui/core";
 import Button from "./shared/Button";
 import ButtonSecondary from "./shared/ButtonSecondary";
 import AddFunds from "./AddFunds";
+import WithdrawFunds from "./WithdrawFunds";
 import Gauge from "./shared/Gauge";
 
 const useStyles = makeStyles((theme) => ({
@@ -17,16 +18,15 @@ const useStyles = makeStyles((theme) => ({
 
 const Home = (props) => {
   const classes = useStyles();
-  const [open, setOpen] = useState(false);
+  const [openAddFunds, setOpenAddFunds] = useState(false);
+  const [openWithdrawFunds, setOpenWithdrawFunds] = useState(false);
   const [currency, setCurrency] = useState("GBP");
   const [angle, setAngle] = useState("rotate(0deg)");
   const [amount, setAmount] = useState("0.00");
 
   useEffect(() => {
     refresh();
-  }, [open]);
-
-  const onClose = (event) => setOpen(false);
+  }, [props]);
 
   const refresh = (event) => {
     axios
@@ -58,12 +58,24 @@ const Home = (props) => {
     <div>
       <Gauge amount={amount} angle={angle} currency={currency} />
       <Container className={classes.buttons}>
-        <Button style={{ margin: "5px" }} onClick={() => setOpen(true)}>
+        <Button style={{ margin: "5px" }} onClick={() => setOpenAddFunds(true)}>
           Add
         </Button>
-        <ButtonSecondary style={{ margin: "5px" }}>Withdraw</ButtonSecondary>
+        <ButtonSecondary
+          style={{ margin: "5px" }}
+          onClick={() => setOpenWithdrawFunds(true)}
+        >
+          Withdraw
+        </ButtonSecondary>
       </Container>
-      <AddFunds onClose={onClose} open={open} />
+      <AddFunds
+        onClose={(event) => setOpenAddFunds(false)}
+        open={openAddFunds}
+      />
+      <WithdrawFunds
+        onClose={(event) => setOpenWithdrawFunds(false)}
+        open={openWithdrawFunds}
+      />
     </div>
   );
 };
